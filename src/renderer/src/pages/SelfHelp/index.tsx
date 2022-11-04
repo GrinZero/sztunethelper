24cd0f452ac3870f0980fafc468ee6f32dc290b8
bugyalu <774933704@qq.com>
2022-11-04 20:17:45 +0800

    chore: add vitest

--- src/renderer/src/pages/SelfHelp/index.tsx
+++ src/renderer/src/pages/SelfHelp/index.tsx
@@ -1,7 +1,11 @@
 import styles from './index.module.scss'
 
 const SelfHelp = () => {
-  return <div className={`${styles.container} main`}>开发中敬请谅解期待</div>
+  return (
+    <div className={`${styles.container} main`}>
+      <div>10.1.20.220</div>
+    </div>
+  )
 }
 
 export default SelfHelp
