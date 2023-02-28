1afb61b28c53058d13fe33db0100ff245172c2ae
GrinZero <GrinZero@users.noreply.github.com>
2023-02-28 04:56:44 +0000

    deploy: a27f68a417d988661af9c54200474ec4484108d2

---
+++ assets/javascript.41d16449.js
@@ -0,0 +1,6 @@
+import{conf as t,language as e}from"./typescript.a92bd208.js";import"./index.2362e026.js";/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.36.0(f86fe937752be8a628c17cecfdfb3287475997ec)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/var o=t,r={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{o as conf,r as language};
