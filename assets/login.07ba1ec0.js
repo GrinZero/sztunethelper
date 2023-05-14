509fd3c151a52c11ee4b1ea7f56b6e44dab801aa
GrinZero <GrinZero@users.noreply.github.com>
2023-05-14 10:10:09 +0000

    deploy: 6307afc2b6784476cb3cf8fafe20332b01f882f1

---
+++ assets/login.07ba1ec0.js
@@ -0,0 +1,1 @@
+import{f as i}from"./index.44e23d23.js";const a=async({username:n,password:o})=>i.send("login",{username:n,password:o},3e3);export{a as l};
--- assets/login.79bc3046.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./index.58c1318c.js";const s=async({username:n,password:o})=>a.send("login",{username:n,password:o},3e3);export{s as l};
